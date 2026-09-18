1b957b2d2e10ad2d37f0d0a5dd5f48cf978a4000
LucasMarques <lucasm.desenvolvedor@gmail.com>
2026-09-18 15:07:52 -0300

    Update link for Aprendizagem Supervisionada

--- data.js
+++ data.js
@@ -300,7 +300,7 @@ const DISCIPLINAS = {
     numero: 4,
     titulo: "Aprendizagem Supervisionada",
     data: "2026-09-04",
-    link: "https://drive.google.com/drive/folders/SEU-LINK-AQUI"
+    link: "https://drive.google.com/drive/folders/1tXyyf4ktalxaE299DSp4GtHvofXzPt3T?usp=drive_link"
   },
   {
     numero: 5,
